653cc561e568509b4ae19f9f8f6ef6496277555d
jamesliu <jamesliu1024@163.com>
2025-02-11 10:48:51 +0800

    initialize

---
+++ src/main/java/seig/ljm/xkckserver/XkckServerApplication.java
@@ -0,0 +1,13 @@
+package seig.ljm.xkckserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XkckServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XkckServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/seig/ljm/xkckserver/XkckServerApplicationTests.java
@@ -0,0 +1,13 @@
+package seig.ljm.xkckserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XkckServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
